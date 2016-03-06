0c467e57924a3abf56772f22516ad40a7f3f72a2
Elhassouny <aaizz.zeroual@gmail.com>
2016-03-06 14:41:01 +0100

    Added files via upload

---
+++ server.R
@@ -0,0 +1,16 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+# 
+# http://www.rstudio.com/shiny/
+#
+
+library(shiny)
+
+shinyServer(
+  function(input, output) {
+    output$oid1 <- renderPrint({input$id1})
+    output$oid2 <- renderPrint({input$id2})
+    output$odate <- renderPrint({input$date})
+  }
+)
